cb53cd68b95772067d4cb7c42dec574d36ff5f20
M1shutqa <143396754+M1shutqa@users.noreply.github.com>
2023-09-24 23:51:43 +0300

    1
    11

--- access/main.js
+++ access/main.js
@@ -3,21 +3,21 @@ scrollLock.disablePageScroll();
 var date = new Date();
 var month = date.getMonth();
 var arr = [
-   'лютого',
-   'березня',
-   'квітня',
-   'травня',
-   'червня',
-   'липня',
-   'серпня',
-   'вересня',
-   'жовтня',
-	 'листопада',
-	 'грудня',
-	 'січня',
+   '02',
+   '03',
+   '04',
+   '05',
+   '06',
+   '07',
+   '08',
+   '09',
+   '10',
+	 '11',
+	 '12',
+	 '01',
 ];
 
-$('.rf_title').text('Данi оновлено ' + date.getDate() + ' ' + arr[month-1] + ' ' + date.getFullYear() +' о ' + (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':' + '' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()));
+$('.rf_title').text('Данi оновлено ' + date.getDate() + '.' + arr[month-1] + '.' + date.getFullYear() +' о ' + (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':' + '' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()));
 
 $('html').css('background', 'linear-gradient(90deg, #aabce8, #cfc6e5, #e2c6e6)');
 
